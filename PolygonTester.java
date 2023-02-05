96065ce4127fd81f2e82c1e5930544d175c0197b
hioem <113708757+hioem@users.noreply.github.com>
2023-02-05 14:41:27 -0800

    Create PolygonTester.java

---
+++ PolygonTester.java
@@ -0,0 +1,15 @@
+public class PolygonTester {
+    public static void main(String[] args) {
+        System.out.println("\nThis is the output with 5 sides, side length of 6.865, and a name of pentagon");
+        Polygon poly1 = new Polygon(5,6.865,"pentagon");
+        System.out.println(poly1);
+
+        System.out.println("\nThis is the output using the default constructor.");
+        Polygon poly2 = new Polygon();
+        System.out.println(poly2);
+
+        System.out.println("\nThis is an example output for an invalid polygon.");
+        Polygon poly3 = new Polygon(1,785,"line");
+        System.out.println(poly3);
+    }
+}
